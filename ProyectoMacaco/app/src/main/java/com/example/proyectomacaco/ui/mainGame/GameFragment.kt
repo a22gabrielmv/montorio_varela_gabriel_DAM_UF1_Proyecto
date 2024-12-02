d32179671579ae1d13f5b04edf5a25942aa4687b
Gabriel <a22gabrielmv@iessanclemente.net>
2024-12-02 10:29:17 +0100

    M20

--- ProyectoMacaco/app/src/main/java/com/example/proyectomacaco/ui/mainGame/GameFragment.kt
+++ ProyectoMacaco/app/src/main/java/com/example/proyectomacaco/ui/mainGame/GameFragment.kt
@@ -12,6 +12,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.bumptech.glide.Glide
@@ -331,6 +332,9 @@ class GameFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
+
+        (activity as? AppCompatActivity)?.supportActionBar?.hide()
+
         gameViewModel.initializeMediaPlayer(requireContext(), R.raw.background_main_game)
 
         startPassiveImprovement()
